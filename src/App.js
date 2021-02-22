9760a714509fa2dbd07993614f2372a188a8bcb5
Peter <pdyuskin@mail.ru>
2021-02-22 15:52:48 +0500

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let amount = parseInt(count)
+        
+        if (count <= 0) {
+            amount = 1
+        }
+        if (count > 8) {
+            amount = 8
+        }
+        setText(data.slice(0,  amount))
+    }
+
+    return (
+        <section className="section-center">
+            <h3>tired of boring lorem ipsum?</h3>
+            <form className="lorem-form" onSubmit={handleSubmit}>
+                <label htmlFor="amount">
+                    paragraphs:
+                </label>
+                <input
+                    value={count}
+                    onChange={(e) => setCount(e.target.value)}
+                    type="number"
+                    name="amount"
+                    id="amount"/>
+                <button type="submit" className="btn">
+                    generate
+                </button>
+            </form>
+            <article className="lorem-text">
+                {text.map((item, index) => {
+                    return <p key={index}>
+                        {item}
+                    </p>
+                })}
+            </article>
+        </section>
+    )
 }
 
 export default App;
